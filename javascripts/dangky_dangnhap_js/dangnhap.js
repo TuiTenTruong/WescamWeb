0c5083a975b2d69ba4f19515642f46e2cfec72fd
maikhang5 <mainhatkhanghug12@gmail.com>
2024-05-02 19:40:10 +0700

    qq

--- javascripts/dangky_dangnhap_js/dangnhap.js
+++ javascripts/dangky_dangnhap_js/dangnhap.js
@@ -78,9 +78,9 @@ form.addEventListener("submit", function (e) {
 
       // Kiểm tra trạng thái trang trước đó từ localStorage
       const currentPage = localStorage.getItem("currentPage");
-      if (currentPage === "chitiet.html") {
+      if (currentPage === "test.html") {
         // Nếu trước đó đang ở trang chi tiết, chuyển hướng đến trang chi tiết
-        window.location.href = "chitiet.html";
+        window.location.href = "test.html";
       } else {
         // Ngược lại, chuyển hướng về trang chính
         window.location.href = "trangchu.html";

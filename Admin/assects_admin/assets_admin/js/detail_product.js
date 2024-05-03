08e39db0f9e2b7210d5dd7a76e17a504e0f0a374
quockiet0505 <dqkiet051104@gmail.com>
2024-05-03 10:34:41 +0700

    kk

--- Admin/assects_admin/assets_admin/js/detail_product.js
+++ Admin/assects_admin/assets_admin/js/detail_product.js
@@ -2,7 +2,7 @@
 //     {
 //         "maSP":"sp001","NhuCau":"Văn phòng","Brand":"HP","TenSP":"Laptop HP Pavilion 14-dv2075TU","Gia":15490000,"Coupon":25,
 
-//         "maDonHang":"dh001", 
+//         "maDonHang":"dh001",
 
 //         "diachi":"Hoả Tiếng, Vị Thanh, Hậu Giang",
 //         "soDT": "0937584978",
